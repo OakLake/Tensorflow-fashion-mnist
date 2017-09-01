735be1693cac7c22675e909b3b2315cf2e069acf
Sammy Hasan <sammy.nm.hasan@gmail.com>
2017-09-01 19:42:06 +0100

    loading data

--- fashion.py
+++ fashion.py
@@ -7,16 +7,17 @@ from mnist_reader import load_mnist
 import os
 import matplotlib.pyplot as plt
 os.system('clear')
-
+print('Library import complete.')
 
 # loading the data
 path = './data/fashion'
 train_images,train_labels = load_mnist(path, kind='train')
 test_images,test_labels = load_mnist(path, kind='t10k')
 
+print('Data loading complete.')
 
-print(train_images.shape)
-print(train_labels.shape)
+print('Train dataset',train_images.shape)
+print('Train labels',train_labels.shape)
 
-print(test_images.shape)
-print(test_labels.shape)
+print('Test dataset',test_images.shape)
+print('Test labels',test_labels.shape)
